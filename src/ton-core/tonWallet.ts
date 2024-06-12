bd241339ac53a10a3cdd641076c934d93e808192
Kirill Nagulin <over90006at9@gmail.com>
2024-06-12 21:25:13 +0300

    new api for Ton interactions

--- src/ton-core/tonWallet.ts
+++ src/ton-core/tonWallet.ts
@@ -1,12 +1,23 @@
 import {Address, TonClient} from 'ton'
+import TonWeb from 'tonweb'
 
-const client = new TonClient({endpoint: process.env.TON_CENTER_ENDPOINT!})
+const endPoint = process.env.TON_CENTER_ENDPOINT!;
+const tonweb = new TonWeb(new TonWeb.HttpProvider(endPoint));
 
 const NANOTON_DIV = 1_000_000_000;
 
-export async function getWalletBalance(walletAddress: string): Promise<number>{
-    const address = Address.parse(walletAddress);
-    const nanoBalance = await client.getBalance(address);
-    const balanceInTon = Number(nanoBalance) /  NANOTON_DIV;
-    return Number(balanceInTon.toFixed(5));
+export async function getWalletBalance(walletAddress: string): Promise<string>{
+    let amount;
+    try{
+        const wallet = new TonWeb.utils.Address(walletAddress);
+        const balance = await tonweb.getBalance(wallet);
+        amount = TonWeb.utils.fromNano(balance);
+        console.log(amount);
+
+    } catch {}
+    // const address = Address.parse(walletAddress);
+    // const nanoBalance = await client.getBalance(address);
+    // const balanceInTon = Number(nanoBalance) /  NANOTON_DIV;
+    // return Number(balanceInTon.toFixed(5));
+    return amount!;
 }
