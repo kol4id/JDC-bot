455d24cc016e0f8fb46f4161351e913d1d20d2b8
Kirill Nagulin <over90006at9@gmail.com>
2024-06-18 17:26:07 +0300

    new functions getTokenHolders and getNftItems

--- src/ton-core/tonWallet.ts
+++ src/ton-core/tonWallet.ts
@@ -1,7 +1,7 @@
 import {Address, TonClient, crc16} from 'ton'
 import TonWeb from 'tonweb'
 
-import  {Api, HttpClient, JettonHolders} from 'tonapi-sdk-js'
+import  {Api, HttpClient, JettonHolders, NftItems} from 'tonapi-sdk-js'
 import Decimal from 'decimal.js';
 
 const endPoint = process.env.TON_CENTER_ENDPOINT!;
@@ -44,85 +44,36 @@ const tokenAbi = {
 };
 
 export async function getWalletBalance(walletAddress: string): Promise<string>{
-    
     let amount: string;
     try{
         const wallet = new TonWeb.utils.Address(walletAddress);
         const balance = await tonweb.getBalance(wallet);
         amount = Number(TonWeb.utils.fromNano(balance)).toFixed(5);
     } catch {}
-
-    // await getTokenHolders()
-    // const address = Address.parse(walletAddress);
-    // const nanoBalance = await client.getBalance(address);
-    // const balanceInTon = Number(nanoBalance) /  NANOTON_DIV;
-    // return Number(balanceInTon.toFixed(5));
     return amount!;
 }
 
 export async function getTokenHolders(address: string): Promise<JettonHolders>{
     return await client.jettons.getJettonHolders(address);
-    // const userAddr = 'UQBvlxYuIhJmzR6pHauU3NMZpq5Xf52TZ6kuUUvPaaU3flcT'
-    // const addr = 'EQDxbNb2Kcqv3xBxje2F3XNH8wDjXBfPaNqF-QIYrUdjUJkO';
-    // const wallet = new TonWeb.utils.Address(addr);
-    // console.log(wallet)
-    // // tonClient.
-    // // const contr = tonClient.open()
-
-    // const 
-    // console.log(holders)
-    // const acc = await client.accounts.getAccountJettonsBalances(userAddr);
-    // const mel = acc.balances.findIndex(bal => bal.jetton.name === 'Melon');
-    // console.log(acc.balances[mel])
-    // const melAdr = new TonWeb.utils.Address(mel?.wallet_address.address!);
-    // console.log(melAdr)
-    // acc.balances.forEach(bal => {
-    //     console.log(bal)
-    // })
-    // console.log(acc);
 }
 
+export async function getNftItems(address: string): Promise<NftItems>{
+    return await client.nft.getItemsFromCollection(address);
+}
 
-const aasda = async() => {
-    const bounc = "UQCq45xZ6OEYlR-J5BSn8R4ddwhFQ5XsptTYB5gRvQ9dVLZA";
-    const hex = '0:aae39c59e8e118951f89e414a7f11e1d7708454395eca6d4d8079811bd0f5d54'
-
-    
-    const nftUser = await client.accounts.getAccountNftItems('UQBHcofnbMpuNKF3TbXntawVzf5C4_dScc_W-fU5ZxVY6lR4');
-    nftUser.nft_items.forEach(nft => {
-        if (nft.collection?.address)
-        console.log(Address.parse(nft.collection?.address));
-    })
-    // console.log(nftUser)
-    // const nftGrouped: Record<string, string[]> = {};
-    // const nft = await client.nft.getItemsFromCollection("EQBQ5N1yWuod9ObxBlOng9GvePdFt856gIYsytx3vxVujpSI");
-
-    // console.log(nft)
-    // nft.nft_items.forEach(_nft => {
-    //     const key = _nft.owner?.address!
-    //     if (!nftGrouped[key]) nftGrouped[key] = [];
-    //     nftGrouped[key].push(_nft.address);
-    // })
-    // console.log(Object.entries(nftGrouped).length)
-
-    // console.log(nftGrouped)
-    // console.log(nftGrouped['0:6895c9904d03b3f09d7a52cc5d172f56c8c3a8cf4227f0c1590dd99471a30b47'])
-    
-
-    // Object.entries(nftGrouped).forEach(([key, value]) => {
-    //     console.log(key)
-    //     console.log(value.length)
-    //     console.log('=======================')
-    // })
-
-    // const wallet = Address.parse(bounc)
-    
-    // console.log(wallet.hash)
-    // wallet.toString('hex')   
+export const aasda = async() => {
+    const adr = "EQD7h40-JLc4Zsbgy-TzIGN3HABmS892ODVSwtD5pP5-ddRb";
+    const nft = await client.nft.getNftItemByAddress(adr);
+    console.log(nft);
 }
 
 export const fromNano = async(nanoBalance: bigint): Promise<number> => {
     const nanoDecimal = new Decimal(nanoBalance.toString());
     const balance = nanoDecimal.div(NANO).toNumber();
     return Number(balance.toFixed(5))
+}
+
+
+export const stringToHexAddr = (addr: string) =>{
+    return Address.parse(addr).toRawString();
 }
