facdd6f64984ca8f705ed1ded73860876fdacc4c
Kirill Nagulin <over90006at9@gmail.com>
2024-06-19 19:12:36 +0300

    tiers added

--- src/states.ts
+++ src/states.ts
@@ -3,6 +3,14 @@ import * as fs from 'fs'
 interface IState{
     allowedUsers: number[]
 }
+enum Tier{
+    bronze = 'bronze',
+    silver = 'silver',
+    gold = 'gold',
+    platinum = 'platinum',
+    diamond = 'diamond'
+}
+
 
 export enum majorStage{
     coinCreate = 'coinCreate',
@@ -18,5 +26,13 @@ export function initState(){
     const file = fs.readFileSync('states.json', 'utf8');
     const state: IState = JSON.parse(file);
     allowedUsers = state.allowedUsers;
+}
+
+export function getTier(points: number){
+    if(points <= 10) return Tier.bronze
+    if(points <= 24) return Tier.silver
+    if(points <= 54) return Tier.gold
+    if(points <= 99) return Tier.platinum
+    return Tier.gold
 }
 
