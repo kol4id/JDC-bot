68798fdd9d73e53ad8e48ef4b5ce92ab9bdfc668
Kirill Nagulin <over90006at9@gmail.com>
2024-06-19 19:13:39 +0300

    now constantly screaning

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { connectDB } from './db/db';
 import { handleCoin } from './bot/coinCommands';
 import { initState } from './states';
 import { handleCollection } from './bot/collectionCommands';
+import path from 'path';
+import { Worker, isMainThread, parentPort } from 'worker_threads';
 import { Screan } from './ton-screan/screan';
 import { CoinRepository } from './db/coin.service';
 import Coins from './db/schemas/coin.schema';
@@ -18,7 +20,8 @@ import Collections from './db/schemas/collection.schema';
 import { aasda } from './ton-core/tonWallet';
 import { WalletRepository } from './db/wallet.service';
 import Wallets from './db/schemas/wallet.schema';
-
+import { UserRepository } from './db/user.service';
+import Users from './db/schemas/user.schema';
 
 async function main(): Promise<void>{
     await initRedisClient();
@@ -27,8 +30,22 @@ async function main(): Promise<void>{
     const screan = new Screan(
         new CoinRepository(Coins), 
         new CollectionRepository(Collections),
-        new WalletRepository(Wallets)
+        new WalletRepository(Wallets),
+        new UserRepository(Users)
     );
+
+    const performTask = async () => {
+        // Используем setInterval для периодического выполнения задачи
+        setInterval(async () => {
+            console.log('Task executed at', new Date().toISOString());
+            try {
+                await screan.screan(); // Убедитесь, что screan.screan() выполняется асинхронно
+            } catch (error) {
+                console.error('Error executing task:', error);
+            }
+        }, 30000);   
+    }
+
 
     try {
         //TODO(kol4id): create controller -> service system 
@@ -52,9 +69,12 @@ async function main(): Promise<void>{
         bot.command('balance', handleBalance);    
         bot.command('coin', handleCoin);
         bot.command('collection', handleCollection);
-        bot.command('screan', screan.screan)
+        // bot.command('screan', screan.screan)
         bot.command('screan1', aasda)
         bot.on('text', handleText);
+
+        await performTask()
+        // startWorker()
         
     } catch (err) {
         console.log(err)
@@ -62,5 +82,22 @@ async function main(): Promise<void>{
     }
     
 }
+
+function startWorker() {
+    const worker = new Worker(path.resolve('src/screanTask.ts'));
+
+    worker.on('error', (error) => {
+        console.error('Worker error:', error);
+    });
+
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(`Worker stopped with exit code ${code}`);
+            startWorker()
+        }
+    });
+}
+
+
 
 main();
