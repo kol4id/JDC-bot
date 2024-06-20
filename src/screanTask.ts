66050260272f89b71b01d4126d91688f3c65c2dc
Kirill Nagulin <over90006at9@gmail.com>
2024-06-20 18:21:32 +0300

    now using await

--- src/screanTask.ts
+++ src/screanTask.ts
@@ -7,6 +7,7 @@ import Users from "./db/schemas/user.schema";
 import Wallets from "./db/schemas/wallet.schema";
 import { UserRepository } from "./db/user.service";
 import { WalletRepository } from "./db/wallet.service";
+import { initRedisClient } from "./ton-connect/storage";
 import { Screan } from "./ton-screan/screan";
 import { parentPort } from 'worker_threads';
 
@@ -19,20 +20,27 @@ const screan = new Screan(
 
 const start = async() => {
     await connectDB();
+    await initRedisClient();
 
     if (parentPort) {
         // Выполняем задачу немедленно при запуске
         // performTask();
     
         // Устанавливаем интервал для выполнения задачи каждые 30 секунд
-        setInterval(performTask, 10000);
+        await performTask()
     }
 }
 
-const performTask = async() => {
-    console.log('Task executed at', new Date().toISOString());
-    await screan.screan()
+const performTask = async () => {
+    // Используем setInterval для периодического выполнения задачи
+    setInterval(async () => {
+        console.log('Task executed at', new Date().toISOString());
+        try {
+            await screan.screan(); // Убедитесь, что screan.screan() выполняется асинхронно
+        } catch (error) {
+            console.error('Error executing task:', error);
+        }
+    }, 15000);   
 }
-
 
 start()
